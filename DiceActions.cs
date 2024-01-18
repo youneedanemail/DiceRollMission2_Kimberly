a052a238a8978080375c633c208f8bc98108c71a
kimberlynoallhunter <kimberlynoallhunter@KIMBERLYNOAB946>
2024-01-17 21:58:13 -0700

    Add project files.

---
+++ DiceActions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollMission2
+{
+    internal class DiceActions
+    {
+        public int[] RollDice(int rollCount) 
+        {
+            int[] rollTotal = new int[13];      // defaults to start with zero
+
+            for (int i = 0; i < rollCount; i++) 
+            {
+                // roll dice - random number
+               
+                Random rnd = new Random();
+
+                int dice1Roll = rnd.Next(1,7);
+                int dice2Roll = rnd.Next(1,7);
+                int diceTotal = dice1Roll + dice2Roll;
+
+                // increment count in array for every value rolled
+                rollTotal[diceTotal]++;           
+            }
+
+            // return array
+            return rollTotal;
+
+
+        }
+    }
+}
+
+
+// for every spot in the array (2-12)
+// calculate the percentage of times that number was rolled
+// print number of percentage
+// for the number of percentage calcuated
+// print one *
+
+
+// int num1 = 13
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿/*Write a .NET console application using C# that simulates the rolling of two 6-sided dice. Use an
+Array to keep track of the number of times that each combination is thrown. In other words,
+keep track of how many times the combination of the two simulated dice is 2, how many times
+the combination is 3, and so on, all the way up through 12.*/
+
+/*Allow the user to choose how many times the “dice” will be thrown. Then, once you have the
+number of rolls, pass that number to a second class that has a method that simulates the roll of
+the dice for the number of times that the user specified. That method in the second class should
+return the array containing the results. In the first class, use that array to print a histogram (using
+the * character) that shows the total percentage each number was rolled. Each * will represent
+1% of the total rolls.*/
+
+using DiceRollMission2;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DiceActions da = new DiceActions();
+        int diceCount = 0;
+
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+        diceCount = int.Parse(Console.ReadLine());
+        Console.WriteLine("");
+
+        int[] rollTotal = da.RollDice(diceCount);
+
+        for (int i = 2; i < rollTotal.Length; i++)
+        {
+            int percentage = ((rollTotal[i] * 100) / diceCount);
+
+            // to print histogram as * 
+
+            string output = new string('*', percentage);
+            Console.WriteLine($"{i}: {output}");                // similar to f'
+        }
+
+
+
+    }
+}
